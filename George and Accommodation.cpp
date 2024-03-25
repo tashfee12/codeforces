1c533b902153a877750a5c0e4f0c1265478f6906
tashfee12 <154910366+tashfee12@users.noreply.github.com>
2024-03-26 03:38:45 +0600

    Add files via upload

---
+++ George and Accommodation.cpp
@@ -0,0 +1,26 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin>>n;
+    int c=0;
+    for(int i=0;i<n;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(b-a>=2)
+        {
+            c++;
+        }
+        else
+            c;
+
+
+    }
+    cout<<c;
+
+
+}
